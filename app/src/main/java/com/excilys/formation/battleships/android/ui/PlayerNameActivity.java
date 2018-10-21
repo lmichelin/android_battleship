fb0c75f1d514e972874d7f4547b5c5164a7f7495
Louis-Marie MICHELIN <louis-marie.michelin@ensta-paristech.fr>
2018-10-21 02:16:26 +0200

    shared preferences & app icon

--- app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
+++ app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
@@ -1,6 +1,7 @@
 package com.excilys.formation.battleships.android.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +14,18 @@ public class PlayerNameActivity extends AppCompatActivity {
 
     private EditText mNameEditText;
 
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferences = getApplicationContext().getSharedPreferences("Pref", MODE_PRIVATE);
+        String name = preferences.getString("player_name", null);
         setContentView(R.layout.activity_player_name);
         mNameEditText = (EditText) findViewById(R.id.edit_player_name);  // bind widgets
+        if (name != null) {
+            mNameEditText.setText(name);
+        }
     }
 
     public void onClickButton(View v) {
@@ -25,6 +33,7 @@ public class PlayerNameActivity extends AppCompatActivity {
         if (name.isEmpty()) {
             Toast.makeText(PlayerNameActivity.this, "Please enter a name", Toast.LENGTH_LONG).show();
         } else {
+            preferences.edit().putString("player_name", name).apply();
             BattleShipsApplication.getmGame().init(name);
         }
     }
